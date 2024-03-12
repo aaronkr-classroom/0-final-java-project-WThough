1a8bf97b1a74b3ea95a24a29f0e4b560cac5122c
WThough <162276803+WThough@users.noreply.github.com>
2024-03-12 17:33:02 +0900

    fin

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,10 +1,32 @@
-
+import java.util.Scanner;
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to shopping Mall!");
-		System.out.println("Welcome to BookMarket!");
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("당신의 이름을 입력하세요: ");
+		String name = input.nextLine();
+		System.out.println("Welcome, " + name + "!");
+		
+		String greeting = "Welcome to Shopping Mall!";
+		String tagline = "Welcome to Book Market!!";
+		
+		System.out.println("*****************");
+		System.out.println("\t" + greeting);
+		System.out.println("\t" + tagline);
+		System.out.println("*****************");
+		
+		System.out.println("1. 고객 정보 확인하기    \t5. 장바구니에 항목 추가하기");
+		System.out.println("2. 장바구니 상품 목록 보기 \t6. 장바구니의 항목 수량 줄이기");
+		System.out.println("3. 장바구니 비우기      \t7. 장바구니의 항목 삭제하기");
+		System.out.println("4. 영수증 표시하기      \t8. 종료");
+		System.out.println("*****************");
+		
+		System.out.println("메뉴 번호를 선택하세요: ");
+		int choice = input.nextInt(); //숫자만 입력
+		System.out.println(choice + "번을 선택했습니다");
+
 	}
 
 }
